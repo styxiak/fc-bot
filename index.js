a9bfed0c05db31da6d0f7e6adc936dd62e3b9cfc
Paweł Chuchmała <pawel.chuchmala@digitalvirgo.pl>
2020-02-27 10:52:57 +0100

    remove ignored files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const fs = require('fs');
 
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 fs.readdir('./events/', (err, files) => {
 });
