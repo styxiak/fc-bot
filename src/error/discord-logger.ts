fa18c1bd06e21ed97f7ce5099eaa80686719f4eb
Paweł Chuchmała <pawel.chuchmala@digitalvirgo.com>
2021-02-21 00:35:02 +0100

    fixes

--- src/error/discord-logger.ts
+++ src/error/discord-logger.ts
@@ -1,9 +1,10 @@
 import {ERROR_CUSTOM_LEVELS, LoggerInterface} from "./logger-interface";
 import {FCBot} from "../FCBot";
-import {MessageEmbed, TextChannel} from "discord.js";
+import {DiscordAPIError, MessageEmbed, TextChannel} from "discord.js";
 import {Color} from "../utils/color";
 import {EmbedUtils} from "../utils/embed-utils";
 import {CHANNEL_ERROR_LOG} from "../types/channel";
+import {Embed} from "../actions/embed";
 
 export class DiscordLogger implements LoggerInterface {
 
@@ -14,15 +15,17 @@ export class DiscordLogger implements LoggerInterface {
     }
 
     debug(msg: any, meta?: any): void {
+        this.embed = EmbedUtils.embed();
         this.embed.setColor(ERROR_CUSTOM_LEVELS.colors.debug);
         this.embed.setTitle('Debug');
         this.sendMessage(msg, meta)
     }
 
     error(msg: any, meta?: any): void {
-        this.embed.setColor(ERROR_CUSTOM_LEVELS.colors.error);
-        this.embed.setTitle('Error');
-        this.sendMessage(msg, meta)
+        let embed = EmbedUtils.embed();
+        embed.setColor(ERROR_CUSTOM_LEVELS.colors.error);
+        embed.setTitle('Error');
+        this.sendMessage(embed, msg, meta)
     }
 
     log(msg: any, meta?: any): void {
@@ -43,23 +46,32 @@ export class DiscordLogger implements LoggerInterface {
         this.sendMessage(msg, meta)
     }
 
-    sendMessage(msg: any, meta?: any): void {
-        this.embed.setDescription(this.toString(msg));
+    sendMessage(embed: MessageEmbed, msg: any, meta?: any): void {
+        let description = this.toString(msg);
+        console.log('description', description);
+        embed.setDescription(description);
         if (meta) {
-            this.embed.addField('Meta', meta);
+            embed.addField('Meta', JSON.stringify(meta).substr(0, 1024));
         }
         let stackTrace = this.getStactTrace(msg);
         if (stackTrace) {
-            this.embed.addField('Stack Trace', stackTrace);
+            embed.addField('Stack Trace', stackTrace.substr(0, 1024));
         }
 
-        FCBot.client.channels.fetch(CHANNEL_ERROR_LOG).then(channel => (channel as TextChannel).send(this.embed));
-        this.embed = EmbedUtils.embed();
+        FCBot.client.channels.fetch(CHANNEL_ERROR_LOG).then(channel => (channel as TextChannel).send(embed));
     }
 
-    toString(msg: any) {
-        if (msg instanceof Object) {
+    toString(msg: any): string {
+        console.log(JSON.stringify(msg));
+        console.log(typeof msg);
+        if (msg instanceof Error) {
+            switch (msg.name) {
+                case 'DiscordAPIError':
+                    return msg.message;
+            }
+        }
 
+        if (msg instanceof Object) {
             return msg.sqlMessage || msg.message;
         }
 
